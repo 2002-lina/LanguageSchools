9108e66854d46fad7abbd63610f9ba53ff1304eb
ЖелезноваАВ@ngknn.local <@ngknn.local>
2021-05-18 15:33:53 +0300

    Добавьте файлы проекта.

---
+++ LanguageSchool/Frame_1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LanguageSchool
+{
+    class Frame_1
+    {
+        public static Frame FR;
+    }
+}
